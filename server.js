e37791dadec6acfdeeefc96dfd4d4981d8cb6de2
mojfazel <fazeli.mojtaba@gmail.com>
2020-12-07 23:51:35 -0500

    change port from 80 to 8080

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
-var server = app.listen(80, () => {
+var server = app.listen(8080, () => {
 	console.log('server is listening on port', server.address().port);
 });
