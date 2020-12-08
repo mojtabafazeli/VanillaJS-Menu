068b72c678678d0bd365e2da2a7838f50b719320
mojfazel <fazeli.mojtaba@gmail.com>
2020-12-07 23:39:21 -0500

    changes port

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
-var server = app.listen(3000, () => {
-    console.log('server is listening on port', server.address().port)
+var server = app.listen(80, () => {
+	console.log('server is listening on port', server.address().port);
 });
-
-
