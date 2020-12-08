1437ea1561d5e61c6e849c61d23267cc25d4bddb
mojfazel <fazeli.mojtaba@gmail.com>
2020-12-07 23:54:31 -0500

    80 again

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
-var server = app.listen(8080, () => {
+var server = app.listen(80, () => {
 	console.log('server is listening on port', server.address().port);
 });
